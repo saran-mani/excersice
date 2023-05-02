b214e7e01dea7e96cfaac80625ce5f8fafb68f65
saran-mani <saranmvdm@gmail.com>
2023-05-02 22:25:24 +0530

    ew

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            </>
+        );
+    }
+}
+ 
+export default Home;
